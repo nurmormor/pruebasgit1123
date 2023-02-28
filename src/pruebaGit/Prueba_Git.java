551102952ff22dfeb2d3f240892703e9935b51d6
nurmo <nurmo@LAPTOP-L2PGKV2U.bbrouter>
2023-02-28 17:58:25 +0100

    Cambio de nombre en la variable partido a partido1.

--- src/pruebaGit/Prueba_Git.java
+++ src/pruebaGit/Prueba_Git.java
@@ -8,29 +8,29 @@ public class Prueba_Git {
 
 	public static void main(String[] args) {
 
-		int partido;
+		int partido1;
 		int golesLocal, golesVisitante;
 		int total1 = 0, total2 = 0, totalX = 0;
-		partido = 1;
-		while (partido <= TOTAL_PARTIDOS) {
-			System.out.println("Introduce los goles del equipo local en el partido " + partido);
+		partido1 = 1;
+		while (partido1 <= TOTAL_PARTIDOS) {
+			System.out.println("Introduce los goles del equipo local en el partido " + partido1);
 			golesLocal = Integer.parseInt(teclado.nextLine());
-			System.out.println("Introduce los goles del equipo visitante en el partido " + partido);
+			System.out.println("Introduce los goles del equipo visitante en el partido " + partido1);
 			golesVisitante = Integer.parseInt(teclado.nextLine());
 			if (golesLocal > golesVisitante) {
-				System.out.println("Resultado de la quiniela en partido " + partido + " es 1");
+				System.out.println("Resultado de la quiniela en partido " + partido1 + " es 1");
 				total1++;
 
 			} else {
 				if (golesVisitante > golesLocal) {
-					System.out.println("Resultado de la quiniela en partido " + partido + " es 2");
+					System.out.println("Resultado de la quiniela en partido " + partido1 + " es 2");
 					total2++;
 				} else {
-					System.out.println("Resultado de la quiniela en partido " + partido + " es X");
+					System.out.println("Resultado de la quiniela en partido " + partido1 + " es X");
 					totalX++;
 				}
 			}
-			partido = partido + 1;
+			partido1 = partido1 + 1;
 		}
 		System.out.println("El porcentaje de 1 es: " + total1 * 100 / TOTAL_PARTIDOS + "%");
 		System.out.println("El porcentaje de 2 es: " + total2 * 100 / TOTAL_PARTIDOS + "%");
