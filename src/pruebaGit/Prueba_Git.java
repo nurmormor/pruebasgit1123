25bbbacb82286373bc4022aeac9f86fa72af9870
nurmo <nurmo@LAPTOP-L2PGKV2U.bbrouter>
2023-02-28 18:06:27 +0100

    Cambio de total_partidos de constante a variable.

--- src/pruebaGit/Prueba_Git.java
+++ src/pruebaGit/Prueba_Git.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Prueba_Git {
 	private static Scanner teclado = new Scanner(System.in);
-	private static final int TOTAL_PARTIDOS = 4;
+	
 
 	public static void main(String[] args) {
-
+		int total_partidos = 4;
 		int partido1;
 		int golesLocal, golesVisitante;
 		int total1 = 0, total2 = 0, totalX = 0;
 		partido1 = 1;
-		while (partido1 <= TOTAL_PARTIDOS) {
+		while (partido1 <= total_partidos) {
 			System.out.println("Bienvenido al partido");
 			System.out.println("Introduce los goles del equipo local en el partido " + partido1);
 			golesLocal = Integer.parseInt(teclado.nextLine());
@@ -33,9 +33,9 @@ public class Prueba_Git {
 			}
 			partido1 = partido1 + 1;
 		}
-		System.out.println("El porcentaje de 1 es: " + total1 * 100 / TOTAL_PARTIDOS + "%");
-		System.out.println("El porcentaje de 2 es: " + total2 * 100 / TOTAL_PARTIDOS + "%");
-		System.out.println("El porcentaje de X es: " + totalX * 100 / TOTAL_PARTIDOS + "%");
+		System.out.println("El porcentaje de 1 es: " + total1 * 100 / total_partidos + "%");
+		System.out.println("El porcentaje de 2 es: " + total2 * 100 / total_partidos + "%");
+		System.out.println("El porcentaje de X es: " + totalX * 100 / total_partidos + "%");
 
 	}
 
